96f503ec203429bcc6904d99e5ee57e17c3bd609
Julien Noel <jncoweb@gmail.com>
2022-10-25 14:10:20 +0200

    maj vercel

--- pages/search.js
+++ pages/search.js
@@ -1,25 +1,32 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { useRouter } from 'next/router'
-import {format} from 'date-fns'
+import { useRouter } from "next/router";
+import { format } from "date-fns";
+import InfoCard from "../components/infoCard";
 
-function Search() {
+function Search({ response }) {
+  const router = useRouter();
+  const { location, startDate, endDate, noOfGuests } = router.query;
 
-    const router = useRouter()
-    const { location, startDate, endDate, noOfGuests } = router.query
+  const formatedStartDate = format(new Date(startDate), "dd MMMM yy");
+  const formatedEndDate = format(new Date(endDate), "dd MMMM yy");
+  const range = `${formatedStartDate} - ${formatedEndDate}`;
 
-    const formatedStartDate = format(new Date(startDate), 'dd MMMM yy')
-    const formatedEndDate = format(new Date(endDate), 'dd MMMM yy')
-    const range = `${formatedStartDate} - ${formatedEndDate}`
+  console.log(response);
 
   return (
     <div className="">
-      <Header placeholder={`${location} | ${range} | ${noOfGuests} guests`}/>
+      <Header placeholder={`${location} | ${range} | ${noOfGuests} guests`} />
 
       <main className="flex">
-        <section className='flex-grow pt-14 px-6'>
-          <p className="text-xs"> 300+ Stays - {range} - for {noOfGuests} guests</p>
-          <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in {location}</h1>
+        <section className="flex-grow pt-14 px-6">
+          <p className="text-xs">
+            {" "}
+            300+ Stays - {range} - for {noOfGuests} guests
+          </p>
+          <h1 className="text-3xl font-semibold mt-2 mb-6">
+            Stays in {location}
+          </h1>
           <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
             <p className="button">Cancellation Flexibility</p>
             <p className="button">Type of Place</p>
@@ -27,6 +34,23 @@ function Search() {
             <p className="button">Rooms and Beds</p>
             <p className="button">More Filters</p>
           </div>
+          <div className='flex flex-col'>
+          {response.map(
+            ({ img, location, title, description, star, price, total }) => (
+              <InfoCard
+                img={img}
+                key={img}
+                location={location}
+                title={title}
+                description={description}
+                star={star}
+                price={price}
+                total={total}
+              />
+            )
+          )}
+          </div>
+          
         </section>
       </main>
       <Footer />
@@ -35,3 +59,14 @@ function Search() {
 }
 
 export default Search;
+
+export async function getServerSideProps() {
+  const searchResult = await fetch("https://www.jsonkeeper.com/b/5NPS");
+  const response = await searchResult.json();
+
+  return {
+    props: {
+      response,
+    },
+  };
+}
